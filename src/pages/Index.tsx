0faf0e79af669432b2d73b6e200e11358dd6f04c
DarkPoolBase <272080896+DarkPoolBase@users.noreply.github.com>
2026-03-28 19:13:51 +0000

    Initial commit from remix

---
+++ src/pages/Index.tsx
@@ -0,0 +1,11 @@
+const Index = () => {
+  return (
+    <iframe
+      src="/aero.html"
+      className="w-full h-screen border-0"
+      title="Aero | Visual Web Development"
+    />
+  );
+};
+
+export default Index;

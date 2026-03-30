4a50f65f2ab7610cbd651873f90eef9d85120b96
DarkPoolBase <272080896+DarkPoolBase@users.noreply.github.com>
2026-03-30 16:22:48 -0400

    feat(orders): register OrderExpiryService and OrderMetricsService in module

--- packages/backend/src/orders/orders.module.ts
+++ packages/backend/src/orders/orders.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { OrderExpiryService } from './services/order-expiry.service';
+import { OrderMetricsService } from './services/order-metrics.service';
 import { Order } from './entities/order.entity';
 import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Order]), AuthModule],
   controllers: [OrdersController],
-  providers: [OrdersService],
-  exports: [OrdersService],
+  providers: [OrdersService, OrderExpiryService, OrderMetricsService],
+  exports: [OrdersService, OrderMetricsService],
 })
 export class OrdersModule {}
